3335406d9f4a5748854d45652dcadc8602ec1307
sugith <sugith.k@gmail.com>
2016-09-27 23:55:05 -0600

    initial

---
+++ readtext.py
@@ -0,0 +1,9 @@
+def main():
+  import Image
+  import pytesseract
+  print pytesseract.image_to_string(Image.open('test.png'))
+
+def test1():
+  import Image
+  import pytesseract
+  print pytesseract.image_to_string(Image.open('bill.jpg'))
